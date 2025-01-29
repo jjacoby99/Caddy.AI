bc02ada651c39a0992c088277f990042262469ae
Josh <joshskisoccer@gmail.com>
2025-01-28 20:16:40 -0800

    Initial commit with files

---
+++ CaddyAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaddyAIApp.swift
+//  CaddyAI
+//
+//  Created by Josh Jacoby on 2025-01-23.
+//
+
+import SwiftUI
+
+@main
+struct CaddyAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CaddyAI
+//
+//  Created by Josh Jacoby on 2025-01-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, World!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
