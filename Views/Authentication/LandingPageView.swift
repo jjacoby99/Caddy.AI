306d3622af41dc45749d4041bc7921db9e0b2afa
Josh <joshskisoccer@gmail.com>
2025-01-28 23:02:41 -0800

    Landing page
    view the user sees upon successful sign in

---
+++ Views/Authentication/LandingPageView.swift
@@ -0,0 +1,20 @@
+//
+//  LandingPageView.swift
+//  CaddyAI
+//
+//  Created by Josh Jacoby on 2025-01-28.
+//
+
+import SwiftUI
+
+struct LandingPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandingPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        LandingPageView()
+    }
+}
